3707a373ecbf4fffe577cb23f23416618b0dbc41
weigo2002 <weigo2002@outlook.com>
2024-12-11 14:48:22 +0800

    use qdrant as vector store

--- pdf_demo.py
+++ pdf_demo.py
@@ -4,8 +4,10 @@ from langchain_community.vectorstores import Chroma
 from langchain_core.embeddings import Embeddings
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
+from langchain_qdrant import QdrantVectorStore, Qdrant
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from torch.nn.functional import embedding
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import VectorParams, Distance
 
 from embedding import get_embeddings
 from llm import get_llm, get_prompt_template
@@ -24,19 +26,17 @@ def process_pdf(file_path: str, embedding: Embeddings):
     chunks = text_splitter.split_text(pdf_text)
     print(f"分割的文本chunk数量: {len(chunks)}")
 
-    vector_store = Chroma.from_texts(
+    qdrant_vector_store = Qdrant.from_texts(
         texts=chunks,
         embedding=embedding,
     )
-
-    return vector_store.as_retriever()
+    return qdrant_vector_store.as_retriever()
 
 if __name__ == "__main__":
     model_name = "qwen2.5"
     embedding_model = "BAAI/bge-small-zh-v1.5"
-
     embedding = get_embeddings(embedding_model)
-    retriever = process_pdf("test.pdf", embedding)
+    retriever = process_pdf("海尔智家股份有限公司2024年第三季度报告.pdf", embedding)
     llm = get_llm(model_name)
 
     prompt = get_prompt_template()
@@ -47,5 +47,5 @@ if __name__ == "__main__":
     } | prompt
       | llm
       | StrOutputParser())
-    result = rag_chain.invoke("请总结一下报告的中心内容")
+    result = rag_chain.invoke("海尔本报告期内净利润，营业利润，经营活动现金流")
     print(result)
